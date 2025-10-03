819173874c0884232bbd97d762af5dcac6dc6076
Micaela Asselborn <asselborn96@gmail.com>
2025-10-03 12:15:51 -0300

    Añadir Bootstrap, routing, hojas de estilo y componentes - Se instala Bootstrap. - Se intala Router-Dom. - Se vuelven a añadir las hojas de estilos. - Se crean los componentes fijos (Navbar y Footer).

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+
+
 import './App.css'
+
+import Home from './pages/Home'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element="{<Home />}" />
+      </Routes>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <footer>
+            <p>Estilos dados con <a href="https://getbootstrap.com/">Bootstrap</a></p>
+            <p>Favicon creado por <a href="https://www.flaticon.com/free-icons/pokemon">Darius Dan - Flaticon</a></p>
+        </footer>
+    )
+}
+
+export default Footer
