f7add0cadbf01a3bde8b3276973388faadd4ca8f
Micaela Asselborn <asselborn96@gmail.com>
2025-10-06 00:32:14 -0300

    Implementar menu hamburguesa en mobiles
    - Tambien se añade la libreria de FontAwesome

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,35 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark} from '@fortawesome/free-solid-svg-icons';
+
 import logo from "../img/logo.png"
 
+
 function Navbar(){
+
+    const [IsOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!IsOpen);
+        
+    }
+
     return(
         <nav>
             <div className="contenedor-logo">
                 <img src={logo} alt="Logo" />
                 <p>PokeMarket</p>
             </div>
-            <div className="navegador">
+            <div className={`desktop ${IsOpen? "menu-desplegado" : "menu-cerrado"}`}>
                 <Link to="/">Inicio</Link>
                 <Link to="/listado">Listado</Link>
                 <Link to="/carrito">Tu carrito</Link>
             </div>
+            <FontAwesomeIcon icon={IsOpen? faXmark : faBars} onClick={toggleMenu} className="mobile"/>
         </nav>
+        
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ function Home(){
     return(
         <main>
             <section>
-                <h1>¡Bienvenidos PokeMarket, tu mercado negro Pokemón de confianza!</h1>
-                <p>¿Pokemones muy dificiles de atrapar? ¿Shinys muy exclusivos? ¿Evasión de impuestos?</p>
+                <h1>¡Bienvenidos a PokeMarket, tu mercado negro Pokemón de confianza!</h1>
+                <p>¿Pokemones muy dificiles de atrapar? ¿No tienes suficientes pokébolas? ¿Impuestos por pokémon atrapado muy altos?</p>
                 <p>¡No te preocupes más!</p>
-                <p>En el Mercado Negro Pokemon puedes encontrar y adquirir facilmente todos los pokemons que necesites sin esfuerzo por un módico precio.</p>
+                <p>En el Mercado Negro Pokemon del Equipo Rocket y Asociados puedes encontrar y adquirir facilmente todos los pokemons que necesites sin esfuerzo alguno por un módico precio.</p>
             </section>
             <hr/>
             <section>
